e7bc8af1fd505bed12fc64f4f1f3d6de0c492663
ADITTYA SAHA <armstersaha8@gmail.com>
2025-03-25 00:29:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Configuration class
 class Config:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    MODEL_NAME = "gemini-2.0-flash"
+    MODEL_NAME = "gemini-2.0-pro-exp-02-05"
     IMAGE_MODEL_NAME = "gemini-pro-vision"
     
     # Streamlit configs
